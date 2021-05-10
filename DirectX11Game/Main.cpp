92480ca14b8bda82e22707bcf04f1400b3e97539
HyeonMyeongJang <gkagm2@gmail.com>
2021-05-10 20:01:31 +0900

    Dynamic lib 암시적, 명시적 링킹 차이

--- DirectX11Game/Main.cpp
+++ DirectX11Game/Main.cpp
@@ -7,10 +7,14 @@
 */
 
 /* Dll 프로젝트 빌드 후 아래와 같은 방식으로 사용 가능
-// Dynamic lib 방식
+// Dynamic lib 방식 (암시적 링킹)
 #include "../Dll/Dll_Func.h"
 #pragma comment(lib, "../x64/Debug/Dll.lib")
 */
+
+
+// Dynamic lib 방식 (명시적 링링)
+typedef int (*pFunc)(int, int);
 
 using namespace std;
 
@@ -25,12 +29,22 @@ int main()
     cout << b << "\n";
     */
 
-    // Dynamic lib방식
+    // Dynamic lib방식 (암시적 링킹)
     /*
     int c = Mul(30, 5);
     cout << c << "\n";
     */
 
+    // Dynamic lib방식 (명시적 링킹)
+    HMODULE hModule = LoadLibrary(L"Dll.dll");
+
+    if (nullptr == hModule)
+        return 0;
+
+    pFunc func = (pFunc)GetProcAddress(hModule, "Mul");
+    int b = func(30, 560);
+    std::cout << b << "\n";
+    FreeLibrary(hModule);
 
     return 0;
 }
