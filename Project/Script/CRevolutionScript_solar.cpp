4c7e3bbdb792667335ea6bb67860ef2a346618c5
HyeonMyeongJang <gkagm2@gmail.com>
2022-02-18 20:02:20 +0900

    RotateAround 구현하기 전 Revolution

--- Project/Script/CRevolutionScript_solar.cpp
+++ Project/Script/CRevolutionScript_solar.cpp
@@ -12,5 +12,11 @@ CRevolutionScript_solar::~CRevolutionScript_solar()
 
 void CRevolutionScript_solar::Update()
 {
-	Transform()->RotateAround(Vector3(0.f, 0.f, 0.f), Vector3::Up, 30.f * DT);
+	if (GetGameObject()->GetParentObject()) {
+		Vector3 vRot = GetGameObject()->GetParentObject()->Transform()->GetLocalRotationDegree();
+		vRot.y += 20.f * DT;
+		GetGameObject()->GetParentObject()->Transform()->SetLocalRotationDegree(vRot);
+	}
+	// TODO (Jang0 : RotateAround 함수 구현 완료 시 이걸로 대체하기
+	//Transform()->RotateAround(Vector3(0.f, 0.f, 0.f), Vector3::Up, 30.f * DT);
 }
